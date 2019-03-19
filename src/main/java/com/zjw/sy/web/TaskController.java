40d70fbee372cfaef56d9deea704726ecc45dfdf
Berserker <dddz97@163.com>
2019-03-19 10:24:22 +0800

    init

--- src/main/java/com/zjw/sy/web/TaskController.java
+++ src/main/java/com/zjw/sy/web/TaskController.java
@@ -33,8 +33,8 @@ public class TaskController {
     /**
      * Description 获取任务列表
      *
-     * @param request
-     * @param query
+     * @param request request
+     * @param query query
      * @return String
      * @author ZhengJiawei
      * @date 2019-03-19 09:22:33
